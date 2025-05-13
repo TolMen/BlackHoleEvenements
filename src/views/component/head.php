1a88ac2a01591f7c7451b33078c0ca70c5f698ba
TolMen Jy <cotton.frachisse@gmail.com>
2025-05-13 11:59:13 +0200

    Création et définition des meta données

---
+++ src/views/component/head.php
@@ -0,0 +1,23 @@
+    <!-- Balises méta essentielles -->
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="mobile-web-app-capable" content="yes" />
+
+    <!-- Informations SEO -->
+    <meta name="keywords" content="événementiel, mariage sur mesure, DJ mariage, location matériel événementiel, concerts, festivals, salons professionnels, soirées étudiantes, remises de diplômes, éclairage, sonorisation, vidéo projection, effets spéciaux, animation musicale, simulation 3D, décorations événementielles, Riom, Clermont-Ferrand, Black Hole Évènements" />
+    <meta name="description" content="Black Hole Évènements, spécialiste de l'événementiel sur mesure : mariages, concerts, festivals, remises de diplômes, salons, soirées étudiantes, location de matériel. De la conception 3D à la réalisation technique, en France et à l’international." />
+    <meta name="author" content="Jessy Frachisse" />
+
+    <!-- Icône du site -->
+    <link rel="icon" type="image/png" sizes="16x16" href="../../../public/assets/favicon.ico" />
+
+    <!-- Feuilles de style externes -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+    <!-- Feuilles de style personnalisées -->
+    <link rel="stylesheet" href="../../../public/css/styleBase.css" />
+    <link rel="stylesheet" href="../../../public/css/styleColor.css" />
+    <link rel="stylesheet" href="../../../public/css/styleComponent.css" />
