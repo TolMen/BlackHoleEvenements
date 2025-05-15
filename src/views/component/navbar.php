7b5b0beb1d270036e042e365d46b350c293f298e
TolMen Jy <cotton.frachisse@gmail.com>
2025-05-15 17:26:23 +0200

    Ajout et modification de liens

--- src/views/component/navbar.php
+++ src/views/component/navbar.php
@@ -17,7 +17,10 @@ $current_page = basename($_SERVER['PHP_SELF']);
                     <a class="custom-nav-link <?php echo ($current_page == 'home.php') ? 'active' : ''; ?>" href="home.php">Accueil</a>
                 </li>
                 <li class="nav-item">
-                    <a class="custom-nav-link <?php echo ($current_page == 'prestations.php') ? 'active' : ''; ?>" href="prestations.php">Prestations</a>
+                    <a class="custom-nav-link <?php echo ($current_page == 'service.php') ? 'active' : ''; ?>" href="service.php">Service</a>
+                </li>
+                <li class="nav-item">
+                    <a class="custom-nav-link <?php echo ($current_page == 'inspiration.php') ? 'active' : ''; ?>" href="inspiration.php">Inspiration</a>
                 </li>
                 <li class="nav-item">
                     <a class="custom-nav-link <?php echo ($current_page == 'faq.php') ? 'active' : ''; ?>" href="faq.php">FAQ</a>
