4a36ff0f934ef4b088798a2fcb68f298dfb3e40d
TolMen Jy <cotton.frachisse@gmail.com>
2025-05-15 17:27:17 +0200

    Création V1 de la section Service

---
+++ src/views/page/sectionHome/sectionService.php
@@ -0,0 +1,72 @@
+<section class="container text-center boxService">
+    <h2 class="section-title">Nos services</h2>
+    <hr class="title-separator">
+    <div class="boxGal">
+        <div class="options">
+            <div class="option active" style="--optionBackground: url(../../../public/assets/img/imgService/imgServiceDecoTextile.jpeg);">
+                <div class="label">
+                    <div class="icon"><img src="../../../public/assets/icon/iconDecoTextile.png" alt=""></div>
+                    <div class="info">
+                        <p class="title">Décoration textile</p>
+                    </div>
+                </div>
+            </div>
+            <div class="option" style="--optionBackground: url(../../../public/assets/img/imgService/imgServiceDecoLumineuse.jpeg);">
+                <div class="label">
+                    <div class="icon"><img src="../../../public/assets/icon/iconDecoLumineuse.png" alt=""></div>
+                    <div class="info">
+                        <p class="title">Décoration lumineuse</p>
+                    </div>
+                </div>
+            </div>
+            <div class="option" style="--optionBackground: url(../../../public/assets/img/imgService/imgServiceEclairage.jpeg);">
+                <div class="label">
+                    <div class="icon"><img src="../../../public/assets/icon/iconEclairage.png" alt=""></div>
+                    <div class="info">
+                        <p class="title">Eclairage</p>
+                    </div>
+                </div>
+            </div>
+            <div class="option" style="--optionBackground: url(../../../public/assets/img/imgService/imgServiceSonorisation.jpeg);">
+                <div class="label">
+                    <div class="icon"><img src="../../../public/assets/icon/iconSonorisation.png" alt=""></div>
+                    <div class="info">
+                        <p class="title">Sonorisation</p>
+                    </div>
+                </div>
+            </div>
+            <div class="option" style="--optionBackground: url(../../../public/assets/img/imgService/imgServiceVideo.jpeg);">
+                <div class="label">
+                    <div class="icon"><img src="../../../public/assets/icon/iconVideo.png" alt=""></div>
+                    <div class="info">
+                        <p class="title">Vidéo</p>
+                    </div>
+                </div>
+            </div>
+            <div class="option" style="--optionBackground: url(../../../public/assets/img/imgService/imgServiceMobilier.jpeg);">
+                <div class="label">
+                    <div class="icon"><img src="../../../public/assets/icon/iconMobilier.png" alt=""></div>
+                    <div class="info">
+                        <p class="title">Mobilier</p>
+                    </div>
+                </div>
+            </div>
+            <div class="option" style="--optionBackground: url(../../../public/assets/img/imgService/imgServiceArtiste.jpeg);">
+                <div class="label">
+                    <div class="icon"><img src="../../../public/assets/icon/iconArtiste.png" alt=""></div>
+                    <div class="info">
+                        <p class="title">Artiste</p>
+                    </div>
+                </div>
+            </div>
+            <div class="option" style="--optionBackground: url(../../../public/assets/img/imgService/imgServiceEffetSpeciaux.jpeg);">
+                <div class="label">
+                    <div class="icon"><img src="../../../public/assets/icon/iconEffetSpeciaux.png" alt=""></div>
+                    <div class="info">
+                        <p class="title">Effets spéciaux</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
