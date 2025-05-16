136c724316c2a145036a91aedaacd144585018c6
TolMen Jy <cotton.frachisse@gmail.com>
2025-05-16 14:52:07 +0200

    Création du responsive pour la section service

--- src/views/page/sectionHome/sectionService.php
+++ src/views/page/sectionHome/sectionService.php
@@ -1,4 +1,5 @@
-<section class="container text-center boxService">
+<!-- VERSION INTERACTIVE (desktop) -->
+<section class="container text-center boxService boxInteractive">
     <h2 class="section-title">Nos services</h2>
     <hr class="title-separator">
     <div class="boxGal">
@@ -69,4 +70,117 @@
             </div>
         </div>
     </div>
+</section>
+
+<!-- VERSION SIMPLIFIÉE (mobile) -->
+<section class="container text-center boxService boxSimpleGallery">
+    <h2 class="section-title">Nos services</h2>
+    <hr class="title-separator">
+
+    <ul class="galleryResponsive">
+        <li>
+            <div class="img-wrapper">
+                <img src="../../../public/assets/img/imgService/imgServiceDecoTextile.jpeg" alt="Décoration textile">
+                <div class="label">
+                    <div class="icon">
+                        <img src="../../../public/assets/icon/iconDecoTextile.png" alt="">
+                    </div>
+                    <div class="info">
+                        <p class="title">Décoration textile</p>
+                    </div>
+                </div>
+            </div>
+        </li>
+        <li>
+            <div class="img-wrapper">
+                <img src="../../../public/assets/img/imgService/imgServiceDecoLumineuse.jpeg" alt="Décoration lumineuse">
+                <div class="label">
+                    <div class="icon">
+                        <img src="../../../public/assets/icon/iconDecoLumineuse.png" alt="">
+                    </div>
+                    <div class="info">
+                        <p class="title">Décoration lumineuse</p>
+                    </div>
+                </div>
+            </div>
+        </li>
+        <li>
+            <div class="img-wrapper">
+                <img src="../../../public/assets/img/imgService/imgServiceEclairage.jpeg" alt="Eclairage">
+                <div class="label">
+                    <div class="icon">
+                        <img src="../../../public/assets/icon/iconEclairage.png" alt="">
+                    </div>
+                    <div class="info">
+                        <p class="title">Eclairage</p>
+                    </div>
+                </div>
+            </div>
+        </li>
+        <li>
+            <div class="img-wrapper">
+                <img src="../../../public/assets/img/imgService/imgServiceSonorisation.jpeg" alt="Sonorisation">
+                <div class="label">
+                    <div class="icon">
+                        <img src="../../../public/assets/icon/iconSonorisation.png" alt="">
+                    </div>
+                    <div class="info">
+                        <p class="title">Sonorisation</p>
+                    </div>
+                </div>
+            </div>
+        </li>
+        <li>
+            <div class="img-wrapper">
+                <img src="../../../public/assets/img/imgService/imgServiceVideo.jpeg" alt="Vidéo">
+                <div class="label">
+                    <div class="icon">
+                        <img src="../../../public/assets/icon/iconVideo.png" alt="">
+                    </div>
+                    <div class="info">
+                        <p class="title">Vidéo</p>
+                    </div>
+                </div>
+            </div>
+        </li>
+        <li>
+            <div class="img-wrapper">
+                <img src="../../../public/assets/img/imgService/imgServiceMobilier.jpeg" alt="Mobilier">
+                <div class="label">
+                    <div class="icon">
+                        <img src="../../../public/assets/icon/iconMobilier.png" alt="">
+                    </div>
+                    <div class="info">
+                        <p class="title">Mobilier</p>
+                    </div>
+                </div>
+            </div>
+        </li>
+        <li>
+            <div class="img-wrapper">
+                <img src="../../../public/assets/img/imgService/imgServiceArtiste.jpeg" alt="Artiste">
+                <div class="label">
+                    <div class="icon">
+                        <img src="../../../public/assets/icon/iconArtiste.png" alt="">
+                    </div>
+                    <div class="info">
+                        <p class="title">Artiste</p>
+                    </div>
+                </div>
+            </div>
+        </li>
+        <li>
+            <div class="img-wrapper">
+                <img src="../../../public/assets/img/imgService/imgServiceEffetSpeciaux.jpeg" alt="Effets spéciaux">
+                <div class="label">
+                    <div class="icon">
+                        <img src="../../../public/assets/icon/iconEffetSpeciaux.png" alt="">
+                    </div>
+                    <div class="info">
+                        <p class="title">Effets spéciaux</p>
+                    </div>
+                </div>
+            </div>
+        </li>
+    </ul>
 </section>
