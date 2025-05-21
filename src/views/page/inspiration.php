fa7046d80e71b43ac9f16e7512860a415b88e1cd
TolMen Jy <cotton.frachisse@gmail.com>
2025-05-21 11:30:38 +0200

    Création et design de la page Inspiration V1

--- src/views/page/inspiration.php
+++ src/views/page/inspiration.php
@@ -1,15 +1,16 @@
 <?php
-
 session_start();
-
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
 
 <head>
-    <!-- Inclusion des balises meta -->
+    <!-- Inclusion des balises meta et autres head communs -->
     <?php include '../component/head.php'; ?>
+
+    <!-- Feuille de style spécifique à la page Inspiration -->
+    <link rel="stylesheet" href="../../../public/css/styleInspiration/styleInspiration.css">
 
     <title>Inspiration - Black Hole Evènements</title>
 </head>
@@ -17,13 +18,23 @@ session_start();
 <body>
 
     <!-- Inclusion de la barre de navigation -->
-    <?php include '../component/navbar.php' ?>
+    <?php include '../component/navbar.php'; ?>
+
+    <!-- Section Inspiration principale -->
+    <?php include 'sectionInspiration/sectionInspiration.php'; ?>
 
     <!-- Inclusion du pied de page -->
-    <?php include '../component/footer.php' ?>
+    <?php include '../component/footer.php'; ?>
 
-    <!-- Liens vers les scripts JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <!-- Librairies JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Scripts personnalisés -->
+    <script src="../../../public/js/filterPhoto.js"></script>
+    <script src="../../../public/js/modalPhoto.js"></script>
+    <script src="../../../public/js/modalClickPhoto.js"></script>
+    <script src="../../../public/js/filterReset.js"></script>
+    <script src="../../../public/js/searchLieu.js"></script>
 
 </body>
 
