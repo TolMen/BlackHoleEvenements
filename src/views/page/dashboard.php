893abd1f420b12c6c46510ec40c8c9a99e6ca118
TolMen Jy <cotton.frachisse@gmail.com>
2025-05-22 15:31:00 +0200

    Ajout en prévision de la fonctionnalité

---
+++ src/views/page/dashboard.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <!-- Inclusion des balises meta -->
+    <?php include '../component/head.php'; ?>
+
+    <title>Dashboard - Black Hole Evènements</title>
+</head>
+
+<body>
+
+    <!-- Inclusion de la barre de navigation -->
+    <?php include '../component/navbar.php' ?>
+
+    <!-- Inclusion du pied de page -->
+    <?php include '../component/footer.php' ?>
+
+    <!-- Liens vers les scripts JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
