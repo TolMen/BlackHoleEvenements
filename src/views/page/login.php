8ed007ddc0f7638e40724f6aed639a6fdb12ab0a
TolMen Jy <cotton.frachisse@gmail.com>
2025-05-22 15:34:36 +0200

    Création et design de la page Connexion V1

--- src/views/page/login.php
+++ src/views/page/login.php
@@ -2,6 +2,7 @@
 
 session_start();
 
+$errorKey = isset($_GET) ? array_key_first($_GET) : null;
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +12,10 @@ session_start();
     <!-- Inclusion des balises meta -->
     <?php include '../component/head.php'; ?>
 
+    <!-- Feuille de style spécifique à la page Login -->
+    <link rel="stylesheet" href="../../../public/css/styleAccount/styleLoginForm.css">
+    <link rel="stylesheet" href="../../../public/css/stylePopUp/stylePopUp.css">
+
     <title>Connexion - Black Hole Evènements</title>
 </head>
 
@@ -18,6 +23,45 @@ session_start();
 
     <!-- Inclusion de la barre de navigation -->
     <?php include '../component/navbar.php' ?>
+
+    <div class="main-container">
+        <!-- Form box -->
+        <div class="box">
+            <span class="borderLine"></span>
+            <!-- Form -->
+            <form method="POST" action="../../control/UserControl/loginUser.php">
+                <h2>Connexion</h2>
+                <!-- Input fields -->
+                <div class="inputBox inputBoxOther">
+                    <input type="text" name="username" maxlength="26" pattern="[a-zA-Z0-9._]{3,26}" title="Seules les lettres, chiffres, '.' et '_' sont autorisés (entre 3 et 26 caractères)" autocomplete="off" required>
+                    <span>Identifiant</span>
+                    <i></i>
+                </div>
+                <div class="inputBox inputBoxOther">
+                    <input type="password" name="password" pattern="[A-Za-zÀ-ÿ0-9.]+" maxlength="15" title="Le mot de passe doit contenir des lettres, des chiffres et uniquement le symboles POINT" autocomplete="off" required>
+                    <span>Mot de passe</span>
+                    <i></i>
+                </div>
+                <!-- End of Input fields -->
+                <div class="links">
+                    <a href="#">Password oublié</a>
+                </div>
+                <input type="submit" name="connexion" value="Se connecter">
+            </form>
+            <!-- End of Form -->
+        </div>
+        <!-- End of Form box -->
+    </div>
+
+    <!-- Popup error -->
+    <?php if (!empty($errorKey)) { ?>
+        <div id="popup" class="popup show">
+            <div class="popup-content">
+                <p class="popup-para">Le compte n'existe pas ou n'a pas encore était créer...</p>
+                <a href="login.php" id="closePopup">Fermer</a>
+            </div>
+        </div>
+    <?php } ?>
 
     <!-- Inclusion du pied de page -->
     <?php include '../component/footer.php' ?>
