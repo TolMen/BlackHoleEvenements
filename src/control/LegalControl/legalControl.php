de8d0cbbe02e306ed99832f9aaa337c30278d4b6
TolMen Jy <cotton.frachisse@gmail.com>
2025-05-22 10:45:19 +0200

    Réunification de la logique et des pages FAQ, Mention legales, Politique de confidentialité en un seul fichier commun

---
+++ src/control/LegalControl/legalControl.php
@@ -0,0 +1,21 @@
+<?php
+include_once '../../model/LegalModel/legalModel.php';
+
+$type = $_GET['type'];
+
+$tableMap = [
+    'faq' => ['table' => 'faq', 'title' => 'Foire aux Questions', 'fields' => ['question', 'answer']],
+    'ml' => ['table' => 'mentionLegale', 'title' => 'Mentions Légales', 'fields' => ['title', 'content']],
+    'pc' => ['table' => 'politiqueConfidentialite', 'title' => 'Politique de Confidentialité', 'fields' => ['title', 'content']]
+];
+
+if (!array_key_exists($type, $tableMap)) {
+    http_response_code(404);
+    exit("Type de page invalide.");
+}
+
+$legalModel = new LegalModel();
+$data = $legalModel->getAll($bdd, $tableMap[$type]['table']);
+$pageTitle = $tableMap[$type]['title'];
+$htmlTitle = $pageTitle . " | Black Hole Évènements";
+$fields = $tableMap[$type]['fields'];
---
+++ src/model/LegalModel/legalModel.php
@@ -0,0 +1,18 @@
+<?php
+include_once '../../control/BDDControl/connectBDD.php';
+
+class LegalModel
+{
+    public function getAll(PDO $bdd, string $table)
+    {
+        $allowedTables = ['faq', 'mentionLegale', 'politiqueConfidentialite'];
+        if (!in_array($table, $allowedTables)) {
+            throw new Exception("Table non autorisée");
+        }
+
+        $sql = "SELECT * FROM {$table} ORDER BY id ASC";
+        $stmt = $bdd->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
