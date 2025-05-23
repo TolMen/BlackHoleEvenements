04aec9112815d73242c577ca9457e17577599c90
TolMen Jy <cotton.frachisse@gmail.com>
2025-05-23 11:57:38 +0200

    Ajout de la logique pour afficher les images dans la page inspiration

---
+++ src/model/ImageModel/galleryImageModel.php
@@ -0,0 +1,19 @@
+<?php
+include_once '../../control/BDDControl/connectBDD.php';
+
+class GalleryImageModel
+{
+    public function getAllImages(PDO $bdd)
+    {
+        $sql = "SELECT 
+                    chemin_img, 
+                    alt, 
+                    filtres_services, 
+                    filtres_themes, 
+                    filtres_lieux 
+                FROM images";
+        $stmt = $bdd->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
