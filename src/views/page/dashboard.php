f7207cb668508dd75e087f9c29c3a430edbc8600
TolMen Jy <cotton.frachisse@gmail.com>
2025-05-30 15:53:42 +0200

    Mise en place d'une struture routeur avec parametres

--- src/views/page/dashboard.php
+++ src/views/page/dashboard.php
@@ -2,6 +2,10 @@
 
 session_start();
 
+include_once '../../control/AdminControl/messagerieControl.php';
+
+
+$isMessagerieView = (isset($_GET['type']) && $_GET['type'] === 'messagerie');
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +15,11 @@ session_start();
     <!-- Inclusion des balises meta -->
     <?php include '../component/head.php'; ?>
 
+    <?php if ($isMessagerieView) { ?>
+        <link rel="stylesheet" href="../../../public/css/styleAdmin/styleMessagerie.css">
+        <link rel="stylesheet" href="../../../public/css/stylePopUp/stylePopUp.css">
+    <?php } ?>
+
     <title>Dashboard - Black Hole Evènements</title>
 </head>
 
@@ -19,11 +28,14 @@ session_start();
     <!-- Inclusion de la barre de navigation -->
     <?php include '../component/navbar.php' ?>
 
-    <!-- Inclusion du pied de page -->
-    <?php include '../component/footer.php' ?>
+    <?php if ($isMessagerieView) {
+        include 'sectionDashboard/sectionMessagerie.php'; 
+    } ?>
 
     <!-- Liens vers les scripts JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <script src="../../../public/js/messagePopup.js"></script>
 
 </body>
 
